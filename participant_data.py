053540c3650339741f6a708c99bd38b393c32a23
Aezurly <alice.vergeau@gmail.com>
2025-05-28 11:49:48 -0400

    update json names

--- participant_data.py
+++ participant_data.py
@@ -5,63 +5,63 @@ class ParticipantData:
 
     def get_name(self) -> str:
         """Returns player name."""
-        return self.data.get("riotIdGameName", "Unknown")
+        return self.data.get("RIOT_ID_GAME_NAME", "Unknown")
 
     def get_total_damage(self) -> int:
         """Returns total damage dealt to champions."""
-        return int(self.data.get("totalDamageDealtToChampions", 0))
+        return int(self.data.get("TOTAL_DAMAGE_DEALT_TO_CHAMPIONS", 0))
 
     def get_team(self) -> str:
         """Returns player's team."""
-        return self.data.get("team", "Unknown")
+        return self.data.get("TEAM", "Unknown")
 
     def get_position(self) -> str:
         """Returns player's position."""
-        return self.data.get("individualPosition", "Unknown")
+        return self.data.get("INDIVIDUAL_POSITION", "Unknown")
 
     def get_kills(self) -> int:
         """Returns number of kills."""
-        return int(self.data.get("championsKilled", 0))
+        return int(self.data.get("CHAMPIONS_KILLED", 0))
 
     def get_deaths(self) -> int:
         """Returns number of deaths."""
-        return int(self.data.get("numDeaths", 0))
+        return int(self.data.get("NUM_DEATHS", 0))
 
     def get_assists(self) -> int:
         """Returns number of assists."""
-        return int(self.data.get("assists", 0))
+        return int(self.data.get("ASSISTS", 0))
     
     def get_champion(self) -> str:
         """Returns champion name."""
-        return self.data.get("skin", "Unknown")
+        return self.data.get("SKIN", "Unknown")
     
     def get_cs(self) -> int:
         """Returns total CS."""
-        return int(self.data.get("minionsKilled", 0)) + int(self.data.get("neutralMinionsKilled", 0))
+        return int(self.data.get("MINIONS_KILLED", 0)) + int(self.data.get("NEUTRAL_MINIONS_KILLED", 0))
 
     def get_cc_time(self) -> int:
         """Returns crowd control time."""
-        return int(self.data.get("totalTimeCrowdControlDealt", 0))
+        return int(self.data.get("TOTAL_TIME_CROWD_CONTROL_DEALT", 0))
 
     def get_vision_score(self) -> int:
         """Returns vision score."""
-        return int(self.data.get("visionScore", 0))
+        return int(self.data.get("VISION_SCORE", 0))
 
     def get_damage_taken(self) -> int:
         """Returns damage taken."""
-        return int(self.data.get("totalDamageTaken", 0))
+        return int(self.data.get("TOTAL_DAMAGE_TAKEN", 0))
 
     def get_total_heal(self) -> int:
         """Returns total healing done."""
-        return int(self.data.get("totalHeal", 0))
+        return int(self.data.get("TOTAL_HEAL", 0))
 
     def get_healing_on_teammates(self) -> int:
         """Returns healing done on teammates."""
-        return int(self.data.get("totalHealOnTeammates", 0))
+        return int(self.data.get("TOTAL_HEAL_ON_TEAMMATES", 0))
     
     def get_gold_spent(self) -> int:
         """Returns total gold spent."""
-        return int(self.data.get("goldSpent", 0))
+        return int(self.data.get("GOLD_SPENT", 0))
 
     def get_damage_per_gold(self) -> float:
         """Returns damage per gold spent."""
@@ -70,7 +70,7 @@ class ParticipantData:
     
     def get_level(self) -> int:
         """Returns player's level."""
-        return int(self.data.get("level", 0))
+        return int(self.data.get("LEVEL", 0))
     
     def get_kda(self) -> float:
         """Returns KDA."""
